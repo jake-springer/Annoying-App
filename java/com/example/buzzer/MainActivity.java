9aeb486a8260f81f40a8c1e3a0be7160301e1b5c
Jake Springer <62592490+jake-springer@users.noreply.github.com>
2024-03-20 22:57:50 -0700

    LEAVE ME ALONE

---
+++ java/com/example/buzzer/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.buzzer;
+
+import android.content.Context;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+
+        findViewById(R.id.buzz_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AudioManager am = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+                am.setStreamVolume(
+                        AudioManager.STREAM_MUSIC,
+                        am.getStreamMaxVolume(AudioManager.STREAM_MUSIC),
+                        0
+                        );
+
+                MediaPlayer sound = MediaPlayer.create(MainActivity.this, R.raw.buzz);
+                sound.start();
+
+
+            }
+        });
+            return insets;
+        });
+    }
+
+
+}
